e13849b96b56fb1a14c2c552a8954538c4f63a1f
Aiman Anizan <anizanaimann1@gmail.com>
2021-10-24 19:38:27 +0800

    `App` component

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+class App extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <h1 className="title">My Todos</h1>
+            </React.Fragment>
+        );
+    }
+}
+
+export default App;
