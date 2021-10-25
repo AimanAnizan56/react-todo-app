fcf41d0b16fd8f264b911ecbb765f6363739406f
Aiman Anizan <anizanaimann1@gmail.com>
2021-10-25 09:23:41 +0800

    Update `app.js`

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import InputField from './components/InputField';
 class App extends Component {
     render() {
         return (
             <React.Fragment>
                 <h1 className="title">My Todos</h1>
+                <InputField />
             </React.Fragment>
         );
     }
