acfc4f87d31e55fc1020cb835cd66ba47f408e50
Aiman Anizan <anizanaimann1@gmail.com>
2021-11-03 14:27:46 +0800

    Add scss classes

--- src/components/InputComponent.jsx
+++ src/components/InputComponent.jsx
@@ -5,22 +5,11 @@ const InputComponent = (props) => {
         props.setInputText(e.target.value);
     };
     return (
-        <div
-            style={{
-                width: '18.75rem',
-                display: 'inline-block',
-            }}
-        >
-            <label htmlFor={props.id} style={{ display: 'block', marginBottom: '1rem' }}>
+        <div className="inputField__component">
+            <label htmlFor={props.id} className="inputField__label">
                 {props.name}
             </label>
-            <input
-                type="text"
-                name={props.id}
-                id={props.id}
-                style={{ padding: '0.5rem' }}
-                onChange={setInput}
-            />
+            <input type="text" name={props.id} id={props.id} onChange={setInput} />
         </div>
     );
 };
--- src/components/InputField.jsx
+++ src/components/InputField.jsx
@@ -4,15 +4,6 @@ import InputComponent from './InputComponent';
 const InputField = ({ setTodos, todos }) => {
     const [name, setName] = useState('');
     const [description, setDescription] = useState('');
-    const card = {
-        backgroundColor: 'hsl(0, 0%, 24%)',
-        borderRadius: '0.625rem',
-        width: '100%',
-        padding: '2rem',
-    };
-    const btn = {
-        backgroundColor: 'hsl(33, 93%, 52%)',
-    };
 
     const buttonHandler = () => {
         if (!(name === '' || description === '')) {
@@ -27,12 +18,10 @@ const InputField = ({ setTodos, todos }) => {
     };
 
     return (
-        <div style={card}>
+        <div className="inputField__card">
             <InputComponent name="Name" id="inputName" setInputText={setName} />
             <InputComponent name="Description" id="inputDesc" setInputText={setDescription} />
-            <button style={btn} onClick={buttonHandler}>
-                Add Todo
-            </button>
+            <button onClick={buttonHandler}>Add Todo</button>
         </div>
     );
 };
