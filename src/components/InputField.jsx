4247bee2eeb7d419fc82cfac0355e19fb161271b
Aiman Anizan <anizanaimann1@gmail.com>
2021-11-03 09:44:16 +0800

    Assign object into array of state

--- src/components/InputField.jsx
+++ src/components/InputField.jsx
@@ -1,22 +1,40 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import InputComponent from './InputComponent';
 
-class InputField extends Component {
-    state = {};
-    render() {
-        const card = {
-            backgroundColor: 'hsl(0, 0%, 24%)',
-            borderRadius: '0.625rem',
-            width: '100%',
-            padding: '2rem',
-        };
-        return (
-            <div style={card}>
-                <InputComponent name="Name" id="inputName" />
-                <InputComponent name="Description" id="inputDesc" />
-            </div>
-        );
-    }
-}
+const InputField = ({ setTodos, todos }) => {
+    const [name, setName] = useState('');
+    const [description, setDescription] = useState('');
+    const card = {
+        backgroundColor: 'hsl(0, 0%, 24%)',
+        borderRadius: '0.625rem',
+        width: '100%',
+        padding: '2rem',
+    };
+    const btn = {
+        backgroundColor: 'hsl(33, 93%, 52%)',
+    };
+
+    const buttonHandler = () => {
+        if (!(name === '' || description === '')) {
+            setTodos([
+                ...todos,
+                {
+                    name: name,
+                    description: description,
+                },
+            ]);
+        }
+    };
+
+    return (
+        <div style={card}>
+            <InputComponent name="Name" id="inputName" setInputText={setName} />
+            <InputComponent name="Description" id="inputDesc" setInputText={setDescription} />
+            <button style={btn} onClick={buttonHandler}>
+                Add Todo
+            </button>
+        </div>
+    );
+};
 
 export default InputField;
