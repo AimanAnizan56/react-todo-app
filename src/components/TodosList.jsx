8258559f36128281f4d23df0b2ab0d919ea2fd5b
Aiman Anizan <anizanaimann1@gmail.com>
2021-11-03 21:26:30 +0800

    Create 'TodoList` component

---
+++ src/components/TodosList.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const TodoList = ({ todos }) => {
+    return (
+        <div>
+            <div>Name Description</div>
+            {/* {todos.map((todo) => (
+                <div>HOOO</div>
+            ))} */}
+        </div>
+    );
+};
+
+export default TodoList;
