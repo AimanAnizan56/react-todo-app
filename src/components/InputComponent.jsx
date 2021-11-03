5f2bf7f995b5c9da7d6a0c8147d444471ec5df7d
Aiman Anizan <anizanaimann1@gmail.com>
2021-11-03 09:43:32 +0800

    add input handler

--- src/components/InputComponent.jsx
+++ src/components/InputComponent.jsx
@@ -1,26 +1,28 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class InputComponent extends Component {
-    render() {
-        return (
-            <div
-                style={{
-                    width: '18.75rem',
-                    display: 'inline-block',
-                }}
-            >
-                <label htmlFor={this.props.id} style={{ display: 'block', marginBottom: '1rem' }}>
-                    {this.props.name}
-                </label>
-                <input
-                    type="text"
-                    name={this.props.id}
-                    id={this.props.id}
-                    style={{ padding: '0.5rem' }}
-                />
-            </div>
-        );
-    }
-}
+const InputComponent = (props) => {
+    const setInput = (e) => {
+        props.setInputText(e.target.value);
+    };
+    return (
+        <div
+            style={{
+                width: '18.75rem',
+                display: 'inline-block',
+            }}
+        >
+            <label htmlFor={props.id} style={{ display: 'block', marginBottom: '1rem' }}>
+                {props.name}
+            </label>
+            <input
+                type="text"
+                name={props.id}
+                id={props.id}
+                style={{ padding: '0.5rem' }}
+                onChange={setInput}
+            />
+        </div>
+    );
+};
 
 export default InputComponent;
