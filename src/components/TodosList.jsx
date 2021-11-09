044a21a3e6efd410ba526653f640533e5c4b209d
Aiman Anizan <anizanaimann1@gmail.com>
2021-11-09 13:52:56 +0800

    Create list from `todos` state

--- src/components/TodosList.jsx
+++ src/components/TodosList.jsx
@@ -1,12 +1,33 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheck, faTrashAlt } from '@fortawesome/free-solid-svg-icons';
 
 const TodoList = ({ todos }) => {
+    const deleteHandler = (e) => {
+        console.log(e.target);
+    };
+    const checkHandler = () => {
+        console.log('checkHandler');
+    };
+
     return (
-        <div>
-            <div>Name Description</div>
-            {/* {todos.map((todo) => (
-                <div>HOOO</div>
-            ))} */}
+        <div className="todos" key={todos.id}>
+            {todos.map((todo) => (
+                <div className="todos__card">
+                    <h1 className="todos__card-name">
+                        <span>{todo.name}</span>
+                        <span>
+                            <button className="trash" onClick={deleteHandler}>
+                                <FontAwesomeIcon icon={faTrashAlt} size="xs" />
+                            </button>
+                            <button className="check" onClick={checkHandler}>
+                                <FontAwesomeIcon icon={faCheck} size="xs" />
+                            </button>
+                        </span>
+                    </h1>
+                    <p className="todos__card-description">{todo.description}</p>
+                </div>
+            ))}
         </div>
     );
 };
