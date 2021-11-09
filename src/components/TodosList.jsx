47b6a63d40e5ea7d2d8636015ff38239bf3790f8
Aiman Anizan <anizanaimann1@gmail.com>
2021-11-09 18:35:50 +0800

    Make `Todos` as a component

--- src/components/TodosList.jsx
+++ src/components/TodosList.jsx
@@ -1,32 +1,15 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCheck, faTrashAlt } from '@fortawesome/free-solid-svg-icons';
+import Todo from './Todos';
 
-const TodoList = ({ todos }) => {
-    const deleteHandler = (e) => {
-        console.log(e.target);
-    };
+const TodoList = ({ todos, setTodos }) => {
     const checkHandler = () => {
         console.log('checkHandler');
     };
 
     return (
-        <div className="todos" key={todos.id}>
+        <div className="todos">
             {todos.map((todo) => (
-                <div className="todos__card">
-                    <h1 className="todos__card-name">
-                        <span>{todo.name}</span>
-                        <span>
-                            <button className="trash" onClick={deleteHandler}>
-                                <FontAwesomeIcon icon={faTrashAlt} size="xs" />
-                            </button>
-                            <button className="check" onClick={checkHandler}>
-                                <FontAwesomeIcon icon={faCheck} size="xs" />
-                            </button>
-                        </span>
-                    </h1>
-                    <p className="todos__card-description">{todo.description}</p>
-                </div>
+                <Todo key={todo.id} todo={todo} setTodos={setTodos} todos={todos} />
             ))}
         </div>
     );
