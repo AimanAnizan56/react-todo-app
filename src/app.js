11a87721fb6c7e84ae5d8003afa290bfa3705edb
Aiman Anizan <anizanaimann1@gmail.com>
2021-11-10 14:45:46 +0800

    Add `filter`

--- src/app.js
+++ src/app.js
@@ -1,14 +1,53 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import FilterDropdown from './components/FilterDropdown';
 import InputField from './components/InputField';
 import TodoList from './components/TodosList';
 
 const App = () => {
     const [todos, setTodos] = useState([]);
+    const [filteredTodos, setFilteredTodos] = useState([]);
+    const [filteredStatus, setFilteredStatus] = useState('all');
+    const [toggle, setToggle] = useState(false);
+
+    useEffect(() => {
+        document.body.addEventListener('click', toggleHandler);
+    }, [toggle]);
+
+    useEffect(() => {
+        filterHandler();
+    }, [todos, filteredStatus]);
+
+    const toggleHandler = () => {
+        if (toggle) {
+            setToggle(false);
+        }
+        document.body.removeEventListener('click', toggleHandler);
+    };
+
+    const filterHandler = () => {
+        switch (filteredStatus) {
+            case 'completed':
+                setFilteredTodos(todos.filter((todo) => todo.completed === true));
+                break;
+            case 'uncompleted':
+                setFilteredTodos(todos.filter((todo) => todo.completed === false));
+                break;
+            default:
+                setFilteredTodos(todos);
+                break;
+        }
+    };
+
     return (
         <React.Fragment>
             <h1 className="title">My Todos</h1>
             <InputField setTodos={setTodos} todos={todos} />
-            <TodoList todos={todos} setTodos={setTodos} />
+            <FilterDropdown
+                toggle={toggle}
+                setToggle={setToggle}
+                setFilteredStatus={setFilteredStatus}
+            />
+            <TodoList todos={todos} setTodos={setTodos} filteredTodos={filteredTodos} />
         </React.Fragment>
     );
 };
